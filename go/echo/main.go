42049fb895ad62d6ba4135086ab504c6f195e6bc
mssigdel01 <m.s.sigdel@gmail.com>
2023-03-07 13:27:05 -0500

    golang echo in progress

--- go/echo/main.go
+++ go/echo/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"crypto/hmac"
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
 	"time"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 const signatureKeyConst = "signature-key"
@@ -43,12 +43,9 @@ func main() {
 }
 
 // Exposed protected handler
-func protectedHandler(c echo.Context) echo.HandlerFunc {
-	// app.logRequest(c, "Protected Handler")
-	return func(c echo.Context) error {
+func protectedHandler(c echo.Context) error {
 		log.Println("This is the protected handler")
 		return c.String(http.StatusOK, "Hello, World!")
-	}
 }
 
 // Timestamp validator
@@ -57,12 +54,12 @@ func TimestampValidator(next echo.HandlerFunc) echo.HandlerFunc {
 		requestTimestamp, err := strconv.ParseInt(c.Request().Header.Get("conceal_timestamp"), 10, 64)
 		currentTimestamp := time.Now().Unix()
 		if err != nil {
-			//app.logRequest(c, "Invalid Timestamp")
+			LogRequest(c, "Invalid Timestamp")
 			c.Response().Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 			return c.String(http.StatusBadRequest, "Invalid Timestamp")
 		}
 		if requestTimestamp-currentTimestamp > 60000 || currentTimestamp-requestTimestamp > 120000 {
-			//app.logRequest(c, "Invalid Timestamp. Timestamp out of range")
+			LogRequest(c, "Invalid Timestamp. Timestamp out of range")
 			c.Response().Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 			return c.String(http.StatusBadRequest, "Invalid Timestamp. Timestamp out of range")
 		}
@@ -73,10 +70,10 @@ func TimestampValidator(next echo.HandlerFunc) echo.HandlerFunc {
 // Signature validator
 func SignatureValidator(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		requestTimestamp, err := c.Request().Header.Get("conceal_timestamp")
-		messageSignature := c.Header.Get("conceal_signature")
+		requestTimestamp := c.Request().Header.Get("conceal_timestamp")
+		messageSignature := c.Request().Header.Get("conceal_signature")
 
-		message := fmt.Sprintf("%s|%s://%s%s", requestTimestamp, "http", c.Request().Host, c.Request().Path)
+		message := fmt.Sprintf("%s|%s://%s%s", requestTimestamp, "http", c.Request().Host, c.Request().URL)
 		hasher := hmac.New(sha256.New, []byte(signatureKeyConst))
 		hasher.Write([]byte(message))
 		sha := fmt.Sprintf("%x", hasher.Sum(nil))
@@ -84,7 +81,7 @@ func SignatureValidator(next echo.HandlerFunc) echo.HandlerFunc {
 		signatureMatch := sha == messageSignature
 
 		if !signatureMatch {
-			//app.logRequest(c, "Signature auth Failed")
+			LogRequest(c, "Signature auth Failed")
 			c.Response().Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 			return c.String(http.StatusUnauthorized, "Invalid Signature")
 		}
@@ -92,16 +89,16 @@ func SignatureValidator(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
-// func logRequest(c echo.Context, tag string) {
-// 	log.Println("Got a new request " + tag)
+func LogRequest(c echo.Context, tag string) {
+	log.Println("Got a new request " + tag)
 
-// 	log.Println(c.Request().URL)
-// 	// log.Println(r.Method)
-// 	// log.Println(r.Header)
-// 	// log.Println(r.URL.RawQuery)
+	log.Println(c.Request().URL)
+	log.Println(c.Request().Method)
+	log.Println(c.Request().Header)
+	log.Println(c.Request().URL)
 
-// 	var body map[string]interface{}
-// 	// json.NewDecoder(r.Body).Decode(&body)
+	var body map[string]interface{}
+	json.NewDecoder(c.Request().Body).Decode(&body)
 
-// 	log.Println(body)
-// }
+	log.Println(body)
+}
