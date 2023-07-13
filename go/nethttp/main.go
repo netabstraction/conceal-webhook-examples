0edaa40230be19f65a6dbcd05ca0e81cee232da9
Paul Chihak <paul.chihak@conceal.io>
2023-07-13 14:31:43 -0500

    update responses to JSON

--- go/nethttp/main.go
+++ go/nethttp/main.go
@@ -19,6 +19,14 @@ const (
 	webhookAddress    = "http://127.0.0.1:8080/webhook"
 )
 
+type Error struct {
+	Error string `json:"error"`
+}
+
+type Status struct {
+	Status string `json:"status"`
+}
+
 func main() {
 
 	// Create a new HTTP server
@@ -35,29 +43,35 @@ func main() {
 
 // Exposed Webhook
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
-
 	requestApiKey := r.Header.Get(apiKeyKeyConst)
 	requestTimestamp := r.Header.Get("conceal-timestamp")
 	requestSignature := r.Header.Get("conceal-signature")
 
+	w.Header().Set("Content-Type", "application/json")
+
 	//Api Key validation
 	if requestApiKey != apiKeyValueConst {
 		log.Printf("Invalid API Key")
-		http.Error(w, "Invalid API Key", http.StatusUnauthorized)
+
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(Error{Error: "Invalid API Key"})
 		return
 	}
 
 	//Timestamp validation
 	if !isValidTimestamp(requestTimestamp) {
 		log.Printf("Invalid Timestamp")
-		http.Error(w, "Invalid Timestamp", http.StatusBadRequest)
+
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error{Error: "Invalid Timestamp"})
 		return
 	}
 
 	//Signature validation
 	if !isValidSignature(requestTimestamp, requestSignature) {
 		log.Printf("Invalid Signature")
-		http.Error(w, "Invalid Signature", http.StatusUnauthorized)
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(Error{Error: "Invalid Signature"})
 		return
 	}
 
@@ -69,6 +83,7 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	// Return a success response
 	log.Printf("OK")
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Status{Status: "OK"})
 	return
 }
 
